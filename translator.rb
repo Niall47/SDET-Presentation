ba85bc8aa5d98e068bfb2dd9110d140f600e65d8
niall james <niall.james@dvla.gov.uk>
2022-07-07 21:11:06 +0100

    cleaned up methods & now using validations.json

--- translator.rb
+++ translator.rb
@@ -8,60 +8,41 @@ class Registration_number
     @area_code = input[0..1]
     @age_identifier = input[2..3]
     @random_section = input[4..6]
-    @value = input.join
+    @value = input
+    @validation_data = JSON.parse(File.read('validations.json'))
   end
 
-  def area_code_valid?(area_code)
+  def area_code_valid?
     # Check our registration number matches a region and does not contain invalid characters
+    region_valid? || !invalid_chars?
+  end
+
+  def region_valid?
+    @validation_data['region'][@area_code[0]] != nil
+  end
+
+  def invalid_chars?
     # We are checking both values for invalid character. May not be needed per brief
-    region_valid?(area_code.first.to_sym) || !invalid_chars?(area_code.to_s)
+    @area_code.include?('I') || @area_code.include?('Q')
   end
 
-  def region_valid?(character)
-    valid_regions[character] != nil
-  end
-
-  def correct_format?(input)
-    # Ensure we are looking at a current style registration
-    # eg. AA11AAA
-    input.match?(/[A-Z]{2}\d{2}[A-Z]{3}/)
-  end
-
-  # E110
-
-  def invalid_chars?(area_code)
-    area_code.include?('I') || area_code.include?('Q')
-  end
-
-  def valid_age?(age_identifier)
+  def valid_age?
+    binding.pry
     # convert the array of chars to an integer
-    age = age_identifier.flatten.join.to_i
-
+    # age = age_identifier.flatten.join.to_i
   end
 
-  def valid_random_characters?(characters)
-    false
-  end
+  def random_characters_valid?
 
+  end
 end
 
-
 # Take an input, if input is not provided or looks invalid we do nothing but complain
-if ARGV.empty? || ARGV.first.chars.count != 7 || !correct_format?(ARGV.first)
+if ARGV.empty? || ARGV.first.chars.count != 7 || !ARGV.first.match?(/[A-Z]{2}\d{2}[A-Z]{3}/)  
   puts "#{ARGV.first} did not look like a registration number"
-  # binding.pry
 else
-  # Clean up input and split into sections
-  input = ARGV.first.upcase.chars
-  area_code = input[0..1]
-  age_identifier = input[2..3]
-  random_section = input[4..6]
-  registration_number = { area_code: area_code, age_identifier: age_identifier, random_section: random_section }
-  binding.pry
-  $validation_data = JSON.parse(File.read('validations.json'))
-
-  # pp $validation_data
-  # puts "Area code valid? #{area_code_valid?(registration_number[:area_code])}"
-  # puts "Age valid? #{valid_age?(registration_number[:age_identifier])}"
-  # puts "Random characters valid? #{valid_random_characters?(registration_number[:random_section])}"
+  registration_number = Registration_number.new(ARGV.first)
+  puts registration_number.region_valid? 
+  # puts registration_number.area_code_valid?
+  # puts registration_number.random_characters_valid?
 end
