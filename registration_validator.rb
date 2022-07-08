13a121f2b030a9c9f4aaa7252f1f351b2e7b00fe
niall james <niall.james@dvla.gov.uk>
2022-07-08 16:58:47 +0100

    bug fixes

--- registration_validator.rb
+++ registration_validator.rb
@@ -5,20 +5,20 @@ require 'json'
 
 class Registration_number
   def initialize(input)
-    @area_code = input[0..1]
+    @region_code = input[0..1]
     @age_identifier = input[2..3]
     @random_section = input[4..6]
     @value = input
     @validation_data = JSON.parse(File.read('validations.json'))
   end
 
-  def area_code_valid?
+  def region_code_valid?
     # Check our registration number matches a region and does not contain invalid characters
-    region_valid? || !invalid_chars?(@area_code)
+    region_valid? || !invalid_chars?(@region_code)
   end
 
   def region_valid?
-    @validation_data['region'][@area_code[0]] != nil
+    @validation_data['region'][@region_code[0]] != nil
   end
 
   def invalid_chars?(string)
@@ -33,36 +33,39 @@ class Registration_number
     !invalid_chars?(@random_section)
   end
 
+  def format_valid?
+    @value.chars.count == 7 && @value.match?(/[A-Z]{2}\d{2}[A-Z]{3}/)
+  end
+
+  def print_age
+    puts @validation_data['age'][@age_identifier] if valid_age? 
+  end
+
   def print_info
-    region = @validation_data['region'][@area_code.chars.first]
+    region = @validation_data['region'][@region_code.chars.first]
     age = @validation_data['age'][@age_identifier]
-    if region && age
-      puts "Your vehicle was registered between #{age} in the #{region} area"
-    end
+    puts "#{@value} was registered between #{age} in the #{region} area" if age && region
   end
 end
 
 
 def check_formatting(input)
-  if input.match?(/[A-Z]{2}\d{2}[A-Z]{3}/) && input.chars.count == 7
-    puts "#{input} looks valid"
-  else
-    puts "#{input} looks invalid"
-  end
+  registration_number = Registration_number.new(input)
+  puts "#{input} appears to be valid" if registration_number.format_valid?
 end
 
 def check_age(input)
   registration_number = Registration_number.new(input)
-  if registration_number.valid_age?
-    puts 'age looks valid'
+  if registration_number.format_valid? && registration_number.valid_age?
+    registration_number.print_age
   else
-    puts 'age looks invalid'
+    puts "Unable to calulcate age for #{input}"
   end
 end
 
 def get_info(input)
   registration_number = Registration_number.new(input)
-  registration_number.print_info
+  registration_number.print_info if registration_number.format_valid?
 end
 
 # Take our command and remove it from ARGV array, this allows us to validate more than one input
@@ -77,17 +80,3 @@ when '-info'
 else
   fail 'Unexpected command: ' + command
 end
-
-
-
-# If input is not provided or looks invalid we do nothing but complain
-# if ARGV.empty? || ARGV.first.chars.count != 7 || !ARGV.first.match?(/[A-Z]{2}\d{2}[A-Z]{3}/)  
-#   puts "#{ARGV.first} did not look like a registration number"
-# else
-#   # Otherwise we print out the results
-#   registration_number = Registration_number.new(ARGV.first)
-#   result = {area_code: registration_number.area_code_valid?,
-#           age: registration_number.valid_age?,
-#           random_characters: registration_number.random_characters_valid?}.to_json
-#   puts result
-# end
